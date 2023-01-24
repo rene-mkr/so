67cd05e001a5c5533f216f4fd3c247b7d352db91
rene-mkr <rene.mkr@gmail.com>
2023-01-24 09:09:50 -0600

    Version inicial

---
+++ Procesos/ordena.c
@@ -0,0 +1,106 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/mman.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <time.h>
+#include <math.h>
+
+void bubble_sort(int *array, int array_size);
+void merge_sort(int *array, float array_size);
+void merge_arrays(int *A,int *L,int leftCount,int *R,int rightCount);
+
+/**
+ * Function that does a bubble sort.
+ * 
+ * @param array         List of integers we want to order.
+ * @param array_size    Size of list we're ordering.
+ *
+ * @return Ordered cluster of what we ordered
+ */
+void bubble_sort(int *array, int array_size) {
+    int sortFlag = 0;
+    int tempHolder = 0;
+
+    do {
+        sortFlag = 1;
+        // printf("\nIteration: ");
+        for (int i = 1; i < array_size; i++) {
+            if (array[i-1] > array[i]) {
+                tempHolder = array[i-1];
+                array[i-1] = array[i];
+                array[i] = tempHolder;
+            } else sortFlag++;
+            // if (i-1 < array_size) printf("%i, ", array[i-1]);
+            // if (i == array_size-1) printf("%i. ", array[i]);
+        }
+        // printf("Made %i moves.", array_size-sortFlag);
+    } while (sortFlag < array_size);
+    // printf("\n");
+}
+
+/**
+ * Function that does a merge sort.
+ * changed by me (Rene)
+ * https://stackoverflow.com/questions/2571049/how-to-sort-in-place-using-the-merge-sort-algorithm
+ */
+void intercala(int* xs, int ini, int m, int fin) {
+
+    int n = fin - ini;
+    int temp[n];
+    int i=ini;
+    int j=m;
+    int k=0;
+
+    while (i < m && j < fin) {
+        if (xs[i] <= xs[j]) {
+            temp[k]=xs[i];
+            k++;
+            i++;
+        }
+        else {
+            temp[k]=xs[j];
+            k++;
+            j++;
+        }
+    }
+    // It finished on one side, make sure both are finished
+    while (i < m) {
+            temp[k]=xs[i];
+            k++;
+            i++;
+    }
+    while (j < fin) {
+            temp[k]=xs[j];
+            k++;
+            j++;
+    }
+    // Copy temp back to the original
+    for (i=ini;i<fin;i++)
+        xs[i]=temp[i];
+}  
+
+
+/**
+ * Function to merge left and right array into the original array. 
+ * 
+ * @param original_array    Array we started with.
+ * @param left              What we consider the left array.
+ * @param left_count        Number of elements in the left array.
+ * @param right             What we consider the right array.
+ * @param right_count       Number of elements in the right array.
+ */
+void merge_arrays(int *original_array,int *left,int left_count,int *right,int right_count) {
+        int i = 0; // i - to mark the index of left subarray (left).
+        int j = 0; // j - to mark the index of right subarray (right).
+    int k = 0; // k - to mark the index of merged subarray (original_array).
+         
+
+        while(i < left_count && j < right_count) {
+                if(left[i]  < right[j]) original_array[k++] = left[i++];
+                else original_array[k++] = right[j++];
+        }
+        while(i < left_count) original_array[k++] = left[i++];
+        while(j < right_count) original_array[k++] = right[j++];
+}
