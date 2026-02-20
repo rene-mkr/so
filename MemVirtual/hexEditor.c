7024aa6466205d1054e8b402f440391a1f37c796
rene-mkr <rene.mkr@gmail.com>
2026-02-20 11:38:08 -0600

    Remove leeChar function from hexEditor.c
    Removed the leeChar function that reads characters from input.

--- MemVirtual/hexEditor.c
+++ MemVirtual/hexEditor.c
@@ -61,24 +61,6 @@ char *mapFile(char *filePath) {
   return map;
 }
 
-int leeChar() {
-  int chars[5];
-  int ch,i=0;
-  nodelay(stdscr, TRUE);
-  while((ch = getch()) == ERR); /* Espera activa */
-  ungetch(ch);
-  while((ch = getch()) != ERR) {
-    chars[i++]=ch;
-  }
-  /* convierte a numero con todo lo leido */
-  int res=0;
-  for(int j=0;j<i;j++) {
-    res <<=8;
-    res |= chars[j];
-  }
-  return res;
-}
-
 
 int visor(char *filename) {
 	
